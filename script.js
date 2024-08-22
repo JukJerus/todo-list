491bd0070b29f7a5a9434bdb4a1ce5d640c11570
shafmaul23 <shafmaul23@gmail.com>
2024-08-22 19:08:10 +0700

    Update Commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputBox = document.getElementById('task');
-
+console.log(inputBox);
 const tasks = document.getElementById('list');
+console.log(tasks);
 
 function addTask() {
   if (inputBox.value === '') {
@@ -15,13 +16,25 @@ function addTask() {
     li.appendChild(span);
     span.innerHTML = '\u00D7';
   }
+  saveData();
 }
 
 tasks.addEventListener('click', function(e) {
   if (e.target.tagName === 'LI') {
     e.target.classList.toggle('checked');
+    saveData();
   }
   else if (e.target.tagName === 'SPAN') {
-    e.target.parentElement.style.display = 'none';
+    e.target.parentElement.remove();
+    saveData();
   }
-});
+}, false);
+
+function saveData() {
+  localStorage.setItem('data', tasks.innerHTML);
+}
+
+function loadData() {
+  tasks.innerHTML = localStorage.getItem('data');
+}
+loadData();
